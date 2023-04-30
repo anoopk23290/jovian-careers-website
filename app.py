4ae2d08ef72a3b6e79be0ab8f8504e1b13641db7
anoopk23290 <61613731+anoopk23290@users.noreply.github.com>
2023-04-30 02:34:32 +0000

    added dynamic data and added api json

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-https://jovian-careers-website.anoopk7.repl.cofrom flask import Flask
-app= Flask(__name__)
+from flask import Flask, render_template,jsonify
+
+app = Flask(__name__)
+
+JOB_LISTING = [{
+  'id': 1,
+  'title': 'Data scientist',
+  "salary": 'Rs 15,00,000',"location": 'Bangalore'
+}, {
+  'id': 2,
+  'title': 'Data engineer',
+  "salary": 'Rs 12,00,000',"location": 'Chennai'
+}, {
+  'id': 1,
+  'title': 'Software developer',
+  "salary": 'Rs 14,00,000',"location": 'Hyderabad'
+}]
+
+
 @app.route("/")
 def hello_world():
-  return("hello world")
+  return render_template('home.html', jobs=JOB_LISTING)
 
-print("hi")
+@app.route("/api/jobs")
+def joblist():
+  return jsonify(JOB_LISTING)
 
-if __name__  == '__main__' :
-  print("hi")
-  app.run(host='0.0.0.0',debug='True')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
